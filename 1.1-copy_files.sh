31403e4edfbe498c5b5815b4b68d8f0e8eaf2d0d
matheusber <lojas@arroway.org>
2025-08-09 08:58:07 -0300

    add 4-release.sh, include Makefile.inc1

--- 1.1-copy_files.sh
+++ 1.1-copy_files.sh
@@ -6,3 +6,4 @@
 
 cp -v $SRC_DIR/build/tools/check-host.py $ZVAULT_HOME/zvio-build/build/tools/check-host.py
 cp -v $SRC_DIR/Makefile $ZVAULT_HOME/zvio-build/Makefile
+cp -v $SRC_DIR/Makefile.inc1 $ZVAULT_HOME/zvio-build/Makefile.inc1
