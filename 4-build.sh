7c61ba3227a8637ccdb71c2ddbd54135f952ef28
matheusber <lojas@arroway.org>
2025-08-09 22:14:13 -0300

    Add 4-build.sh, minor changes and fix commit message :)

---
+++ 4-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Run make target to install initial needed packages
+
+. env.sh
+
+# Script to build base. Logs time it starts and finishes.
+
+log_print $TAG_SRC $LOG $DATE Inicio
+
+pwd
+
+#mv -v $ZVAULT_HOME/zvio-build/build/profiles/freenas $ZVAULT_HOME/zvio-build/build/profiles/zvault
+#ln -sf $ZVAULT_HOME/zvio-build/build/profiles/freenas $ZVAULT_HOME/zvio-build/build/profiles/zvault
+
+# Change dir
+cd $ZVAULT_HOME
+cd zvio-build
+
+make freenas
+
+log_print $TAG_SRC $LOG $DATE Final
